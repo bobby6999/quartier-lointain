7e478e3ec93922c050e64601ce09f3c7e77f2717
bobby6999 <122967630+bobby6999@users.noreply.github.com>
2024-11-13 14:40:38 +0100

    updte animation papillon

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,11 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 /*^^^^^^^^^^^^^^^^ ^^^^^^^^ ^^^^^^^^installation gsap ^^^^^^^^ ^^^^^^^^^ ^^^^^^^^ ^^^^^^^^^^^^^^^^*/
 
-/* first logo  */
+/* first logo ----------------------------*/
 function disableScroll() {
   document.body.style.overflow = "hidden";
 }
+
 function enableScroll() {
   document.body.style.overflow = "";
 }
@@ -17,4 +18,43 @@ function enableScroll() {
 gsap
   .timeline()
   .call(disableScroll)
-  .to(".first-logo", { opacity: 0, duration: 4 });
+  .to(".first-logo", { opacity: 0, duration: 2 })
+  .to(".first-logo", { opacity: 0, duration: 2 })
+  .to(".first-logo", { duration: 2 })
+  .to("#papillon", { x: 100, duration: 1 })
+  .to("#papillon", { x: 200, y: -20, duration: 1 })
+  .to("#papillon", { x: 300, y: -80, duration: 1 })
+  .to("#papillon", { x: 400, y: -60, duration: 1 })
+  .to("#papillon", { x: 500, y: 0, duration: 1 })
+  .to("#papillon", { scale: 1.5, duration: 1 })
+  .to("#papillon", { scale: 1, duration: 1 })
+  .to("#papillon", { scale: 1.5, duration: 1 })
+  .to("#papillon", { scale: 1, duration: 1 })
+  .to("#papillon", { scale: 1.5, duration: 1 })
+  .to("#papillon", { scale: 1, duration: 1 })
+  .to("#papillon", { scale: 1.5, duration: 1 })
+  .to("#papillon", { scale: 1, duration: 1 })
+  .to("#papillon", { x: 800, y: -100, duration: 1, delay: 1 })
+  .to("#papillon", { x: 900, y: -200, duration: 1 })
+  .call(enableScroll);
+
+/* papillon--------------------------------*/
+
+function startPapillonAnimation() {
+  var tl = gsap.timeline();
+  tl.to("#papillon", { x: 100, duration: 1 });
+  tl.to("#papillon", { x: 200, y: -20, duration: 1 });
+  tl.to("#papillon", { x: 300, y: -80, duration: 1 });
+  tl.to("#papillon", { x: 400, y: -60, duration: 1 });
+  tl.to("#papillon", { x: 500, y: 0, duration: 1 });
+  tl.to("#papillon", { scale: 1.5, duration: 1 });
+  tl.to("#papillon", { scale: 1, duration: 1 });
+  tl.to("#papillon", { scale: 1.5, duration: 1 });
+  tl.to("#papillon", { scale: 1, duration: 1 });
+  tl.to("#papillon", { scale: 1.5, duration: 1 });
+  tl.to("#papillon", { scale: 1, duration: 1 });
+  tl.to("#papillon", { scale: 1.5, duration: 1 });
+  tl.to("#papillon", { scale: 1, duration: 1 });
+  tl.to("#papillon", { x: 800, y: -100, duration: 1, delay: 1 });
+  tl.to("#papillon", { x: 900, y: -200, duration: 1 });
+}
